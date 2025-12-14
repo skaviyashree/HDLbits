1f0db430096e06ed88096b4b1b4f0d3431a2b205
skaviyashree <skaviyashree21@gmail.com>
2025-12-15 01:28:38 +0530

    Update module_name.v with top_module definition

--- HDLbits/Verilog_Language/Module Hierachy/module_name.v
+++ HDLbits/Verilog_Language/Module Hierachy/module_name.v
@@ -10,4 +10,15 @@ input in4	d
 You are given the following module:
 module mod_a ( output out1, output out2, input in1, input in2, input in3, input in4);
 */
+module top_module ( 
+    input a, 
+    input b, 
+    input c,
+    input d,
+    output out1,
+    output out2
+);
 
+mod_a inst(.out1(out1),.out2(out2),.in1(a),.in2(b),.in3(c),.in4(d) );
+
+endmodule
